2017cc8f937098a2c2c595654369204a37341f0c
danny-20 <57652772+danny-20@users.noreply.github.com>
2023-06-27 12:16:54 +0530

    new erquest added and starred component populated and logic added

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,10 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
 import { useStarredShows } from '../lib/UseStarredShows';
+import { getShowsById } from '../api/tvmaze';
+import ShowsGrid from '../components/shows/ShowsGrid';
 
 const Starred = () => {
-  const [starredShows] = useStarredShows();
+  const [starredShowsIds] = useStarredShows();
 
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: () =>
+      getShowsById(starredShowsIds).then(result =>
+        result.map(show => ({ show }))
+      ),
+
+    refetchOnWindowFocus: false,
+  });
   //starred show
-  return <div>Starred Page, starred {starredShows.length}</div>;
+  if (starredShows?.length === 0) {
+    return <div>No Shows ere starred</div>;
+  }
+
+  if (starredShows?.length > 0) {
+    return <ShowsGrid shows={starredShows} />;
+  }
+
+  if (starredShowsError) {
+    return <div>Error Occured: {starredShowsError.message}</div>;
+  }
+  return <div>Shows are loading</div>;
 };
 
 export default Starred;
